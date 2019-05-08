d28e6ed1e1c827cb4c8c6837a90345629d57db2c
Nelson Trindade <nelsontr@users.noreply.github.com>
2019-05-08 14:50:07 +0100

    ...

--- Projeto#2/functions.h
+++ Projeto#2/functions.h
@@ -7,43 +7,10 @@
 #define MAX_EMAIL 512
 #define MAX_PHONE 64
 
-/*Structurs*/
-typedef struct{
-  char *name, *email, *phone;
-} *contacts;
-
-typedef struct node{
-  contacts contact;   /*VERIFICAR SE E' NECESSARIO COLOCAR POINTER*/
-  struct node *next;
-} *link;
-
-char* input(char buffer[]){
-  char *x = (char*) malloc(sizeof(char) * (strlen(buffer)+1));
-  strcpy(x,buffer);
-  return x;
-}
-
-link add_contact(link head, char name[], char email[], char phone[]){
-  contacts contact_aux = malloc(sizeof(contacts));
-  link y, x = malloc(sizeof(struct node));
-  contact_aux->name = input(name);
-  contact_aux->email = input(email);
-  contact_aux->phone = input(phone);
 
 
-  x->contact = contact_aux;
-  x->next = NULL;
+char* input(char buffer[]);
 
-/* ADD TO THE END */
-  if (head==NULL)
-    return x;
-  for (y=head; y->next != NULL; y = y->next);
-  y->next = x;
-  return head;
-}
+link add_contact(link head, char name[], char email[], char phone[]);
 
-void list_contact(link head){
-  link t;
-  for (t=head;t!=NULL; t=t->next)
-    printf("%s %s %s\n", t->contact->name,t->contact->email,t->contact->phone);
-}
+void list_contact(link head);
---
+++ Projeto#2/linkedlist.c
@@ -0,0 +1,42 @@
+#include "functions.h"
+
+/*Structurs*/
+typedef struct{
+  char *name, *email, *phone;
+} *contacts;
+
+typedef struct node{
+  contacts contact;   /*VERIFICAR SE E' NECESSARIO COLOCAR POINTER*/
+  struct node *next;
+} *link;
+
+char* input(char buffer[]){
+  char *x = (char*) malloc(sizeof(char) * (strlen(buffer)+1));
+  strcpy(x,buffer);
+  return x;
+}
+
+link add_contact(link head, char name[], char email[], char phone[]){
+  contacts contact_aux = malloc(sizeof(contacts));
+  link y, x = malloc(sizeof(link));
+  contact_aux->name = input(name);
+  contact_aux->email = input(email);
+  contact_aux->phone = input(phone);
+
+
+  x->contact = contact_aux;
+  x->next = NULL;
+
+/* ADD TO THE END */
+  if (head==NULL)
+    return x;
+  for (y=head; y->next != NULL; y = y->next);
+  y->next = x;
+  return head;
+}
+
+void list_contact(link head){
+  link t;
+  for (t=head;t!=NULL; t=t->next)
+    printf("%s %s %s\n", t->contact->name,t->contact->email,t->contact->phone);
+}
