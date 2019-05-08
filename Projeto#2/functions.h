b87ef9585163a42ea76e21cd8d75a0d9d66ab05e
Nelson Trindade <48919500+nelsontr@users.noreply.github.com>
2019-05-08 14:20:53 +0100

    Add files via upload

--- Projeto#2/functions.h
+++ Projeto#2/functions.h
@@ -17,40 +17,27 @@ typedef struct node{
   struct node *next;
 } *link;
 
-// GLOBAL VARIABLE
-link head;
-
-
-
-
-void init(){
-  top = NULL;
-}
-
-init is_empty(){
-  return top==NULL;
-}
-
-char* input(char buffer){
-  char *x = (char*) malloc(sizeof(char) * (strlen(buffer)+1);
+char* input(char buffer[]){
+  char *x = (char*) malloc(sizeof(char) * (strlen(buffer)+1));
   strcpy(x,buffer);
   return x;
 }
 
 link add_contact(link head, char name[], char email[], char phone[]){
-  Contacts contact_aux;
-  link x = malloc(sizeof(struct node));
-  link y;
-  contact_aux.name = input(name);
-  contact_aux.email = input(email);
-  contact_aux.phone = input(phone);
+  contacts contact_aux = malloc(sizeof(contacts));
+  link y, x = malloc(sizeof(struct node));
+  contact_aux->name = input(name);
+  contact_aux->email = input(email);
+  contact_aux->phone = input(phone);
+
+
   x->contact = contact_aux;
   x->next = NULL;
 
-//ADD TO THE END
+/* ADD TO THE END */
   if (head==NULL)
     return x;
-  for (y=head; y->next != NULL; y = y->next)
+  for (y=head; y->next != NULL; y = y->next);
   y->next = x;
   return head;
 }
@@ -58,36 +45,5 @@ link add_contact(link head, char name[], char email[], char phone[]){
 void list_contact(link head){
   link t;
   for (t=head;t!=NULL; t=t->next)
-    printf("%s , %s, %s\n",t->contact->name,t->contact->email,t->contact->phone);
-}
-
-
-
-
-// link NEW(const char* buffer){
-//   link x = malloc(sizeof(struct node));
-//   x->text = malloc(sizeof(char)*(strlen(buffer)+1));
-//   strcpy(x->text, buffer);
-//   x->next = NULL;
-//   return x;
-// }
-
-void list_contact(){
-
-}
-
-void search_contact(){
-
-}
-
-void delete_contact(){
-
-}
-
-void change_email(){
-
-}
-
-void search_email(){
-
+    printf("%s %s %s\n", t->contact->name,t->contact->email,t->contact->phone);
 }
--- Projeto#2/main.c
+++ Projeto#2/main.c
@@ -7,32 +7,27 @@
 
 #include "functions.h"
 
+
+/* GLOBAL VARIABLE */
+link head;
+
+
 int main(){
-  char name[MAX_NAME],email[MAX_EMAIL],phone[MAX_PHONE];
+  char command,name[MAX_NAME],email[MAX_EMAIL],phone[MAX_PHONE];
   while (1){
     strcpy(name,""); strcpy(email,""); strcpy(phone,"");
     command=getchar();
     switch(command){
       case 'a': /*Add a Contact*/
         scanf(" %s %s %s\n",name,email,phone);
-        head = add_contact(head,name,email,phone);
+        head=add_contact(head,name,email,phone);
         break;
       case 'l': /*Displays all contacts by order of creation*/
         list_contact(head);
         break;
-      case 'p': /*Searches Contact*/
-        search_contact();
-        break;
-      case 'r': /*Removes a Contact*/
-        delete_contact();
-        break;
-      case 'e': /*Changes the email of a Contact*/
-        change_email();
-        break;
-      case 'c': /*How many contacts as the same email*/
-        search_email();
-        break;
+
       case 'x': /*Exits the Program*/
+        free(head);
         exit(0);
         break;
     }
