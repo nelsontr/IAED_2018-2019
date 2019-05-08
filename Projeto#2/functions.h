6a1bb1d4c297fa4c20282bcbdcd63f3c4e441920
Nelson Trindade <nelsontr@users.noreply.github.com>
2019-05-08 13:14:02 +0100

    Update functions.h

--- Projeto#2/functions.h
+++ Projeto#2/functions.h
@@ -9,20 +9,68 @@
 
 /*Structurs*/
 typedef struct{
-    char name[MAX_NAME], email[MAX_EMAIL], phone[MAX_PHONE];
-} Contacts;
+  char *name, *email, *phone;
+} *contacts;
 
-struct node{
-  Contacts contact;
+typedef struct node{
+  contacts contact;   /*VERIFICAR SE E' NECESSARIO COLOCAR POINTER*/
   struct node *next;
-};
+} *link;
+
+// GLOBAL VARIABLE
+link head;
 
 
 
 
-void add_contact(){
-
+void init(){
+  top = NULL;
 }
+
+init is_empty(){
+  return top==NULL;
+}
+
+char* input(char buffer){
+  char *x = (char*) malloc(sizeof(char) * (strlen(buffer)+1);
+  strcpy(x,buffer);
+  return x;
+}
+
+link add_contact(link head, char name[], char email[], char phone[]){
+  Contacts contact_aux;
+  link x = malloc(sizeof(struct node));
+  link y;
+  contact_aux.name = input(name);
+  contact_aux.email = input(email);
+  contact_aux.phone = input(phone);
+  x->contact = contact_aux;
+  x->next = NULL;
+
+//ADD TO THE END
+  if (head==NULL)
+    return x;
+  for (y=head; y->next != NULL; y = y->next)
+  y->next = x;
+  return head;
+}
+
+void list_contact(link head){
+  link t;
+  for (t=head;t!=NULL; t=t->next)
+    printf("%s , %s, %s\n",t->contact->name,t->contact->email,t->contact->phone);
+}
+
+
+
+
+// link NEW(const char* buffer){
+//   link x = malloc(sizeof(struct node));
+//   x->text = malloc(sizeof(char)*(strlen(buffer)+1));
+//   strcpy(x->text, buffer);
+//   x->next = NULL;
+//   return x;
+// }
 
 void list_contact(){
 
